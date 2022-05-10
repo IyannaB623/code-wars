9bfbf4ccd86d7ed573e9e6ff7e41d92cfe529bb3
Iyanna Blackburn <87724081+IyannaB623@users.noreply.github.com>
2022-05-10 00:09:07 -0700

    Create stringEndsWith.js

---
+++ 7 kyu/stringEndsWith.js
@@ -0,0 +1,19 @@
+/*Problem
+Complete the solution so that it returns true if the first argument(string) passed in ends with the 2nd argument (also a string).
+
+Examples:
+
+solution('abc', 'bc') // returns true
+solution('abc', 'd') // returns false
+*/
+
+//My Solution
+function solution(str, ending){
+  
+  if(str.includes(ending) && ending[ending.length -1]===str[str.length -1] || str.includes(ending) && ending===''){
+      return(true) 
+    } else {
+      return(false)  
+    }
+   
+}
