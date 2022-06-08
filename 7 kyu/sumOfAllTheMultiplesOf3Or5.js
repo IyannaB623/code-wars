2263a3dc2506c62959f57f928db4df1f2898522b
Iyanna Alani <87724081+IyannaB623@users.noreply.github.com>
2022-06-08 09:54:00 -0700

    Create sumOfAllTheMultiplesOf3Or5.js

---
+++ 7 kyu/sumOfAllTheMultiplesOf3Or5.js
@@ -0,0 +1,26 @@
+/*Problem
+Your task is to write function findSum.
+
+Upto and including n, this function will return the sum of all multiples of 3 and 5.
+
+For example:
+
+findSum(5) should return 8 (3 + 5)
+
+findSum(10) should return 33 (3 + 5 + 6 + 9 + 10)
+*/
+
+//My Solution
+function findSum(n) {
+  
+  let multiples = 0
+  
+  for(let i=1; i<n+1; i++){
+    if(i%3===0 || i%5===0){
+      multiples += i 
+    }
+    }
+    
+    return multiples
+  
+}
